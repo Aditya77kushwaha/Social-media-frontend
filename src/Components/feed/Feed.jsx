e4918d068f52d82013f49ef8a9cda22506517712
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-05-27 14:23:43 +0530

    Done Share Component in Feed and re-spelled assets dir.

--- src/Components/feed/Feed.jsx
+++ src/Components/feed/Feed.jsx
@@ -1,7 +1,12 @@
+import Share from "../share/Share";
 import "./feed.css";
 
-const Feed = () => {
-  return <div className="feed">Feed</div>;
-};
-
-export default Feed;
+export default function Feed() {
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+      </div>
+    </div>
+  );
+}
