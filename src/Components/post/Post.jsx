5c52bcd3848e14a1920b7452f03aac72fe5c68be
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-05-27 15:09:50 +0530

    Post Component in Feed.

---
+++ src/Components/post/Post.jsx
@@ -0,0 +1,39 @@
+import "./post.css";
+import { MoreVert } from "@material-ui/icons";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src="assets/person/person1.jpg"
+              alt=""
+            />
+            <span className="postUsername">Aditya</span>
+            <span className="postDate">5 minutes ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">New text</span>
+          <img className="postImg" src="assets/post/post1.jpg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/dislike.jpg" alt="" />
+            <span className="postLikeCounter">5 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
