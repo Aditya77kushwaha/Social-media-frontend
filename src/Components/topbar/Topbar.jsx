a0de262111666d4753f498f2e208bac60b5a7233
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-06-01 16:14:21 +0530

    Final, linked backend with frontend using axios, renamed assets.

--- src/Components/topbar/Topbar.jsx
+++ src/Components/topbar/Topbar.jsx
@@ -1,11 +1,18 @@
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Topbar() {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">Social Media</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Social Media</span>
+        </Link>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
@@ -35,11 +42,17 @@ export default function Topbar() {
             <span className="topbarIconBadge">1</span>
           </div>
         </div>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN7njwJYxu4mNe-gKO_JO2V3alorDOSGaR_Q&usqp=CAU"
-          alt=""
-          className="topbarImg"
-        />
+        <Link to={`/profile/${user.username}`}>
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
       </div>
     </div>
   );
