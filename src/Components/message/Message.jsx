8c07de4ddbec110b78ae836a3815212e59e11674
aditya77kushwaha <adityakushwaha2912@gmail.com>
2022-07-31 21:26:42 +0530

    fixes user pfp in messenger

--- src/Components/message/Message.jsx
+++ src/Components/message/Message.jsx
@@ -1,13 +1,22 @@
 import "./message.css";
 import { format } from "timeago.js";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
 
 export default function Message({ message, own }) {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
   return (
     <div className={own ? "message own" : "message"}>
       <div className="messageTop">
         <img
           className="messageImg"
-          src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          src={
+            user.profilePicture
+              ? PF + user.profilePicture
+              : "https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          }
           alt=""
         />
         <p className="messageText">{message.text}</p>
